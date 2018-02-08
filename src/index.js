a6d297d425b7bcf982f55f5150f62a5b445e1a69
Anubhav Saini <anusaini@paypal.com>
2018-02-08 09:38:52 +0530

    Version 3
    Make run function accept commander, logger and whenDone params
    Update tests

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ const chalk = require('chalk')
 const program = require('commander')
 const { parsePackage } = require('parse-package')
 
-function run() {
-    program
-        .arguments('<filename>.json')
-        .action(function(file) {
-            console.log(`File: ${chalk.bold.cyan(file)}`)
-            const out = parsePackage(file)
-            Object.keys(out).forEach(k => {
-                console.log(chalk.magenta('key:'), k, chalk.cyan('val:'), out[k])
-            })
-            return out
+const run = (commander, logFn, whenDone) => {
+    const executor = commander || program
+    const logger = logFn || console.log.bind(console)
+    const onAction = file => {
+        logger(`File: ${chalk.bold.cyan(file)}`)
+        const out = parsePackage(file)
+        Object.keys(out).forEach(k => {
+            logger(chalk.magenta('key:'), k, chalk.cyan('val:'), out[k])
         })
-        .parse(process.argv)
+        whenDone = Array.isArray(whenDone) || [whenDone]
+        whenDone.forEach(done => done(out))
+        return out
+    }
+    const desc = '<filename>.json'
+
+    executor.arguments(desc).action(onAction).parse(process.argv)
 }
 
 run()
--- src/index.spec.js
+++ src/index.spec.js
@@ -7,4 +7,36 @@ describe('parse-package-cli', () => {
     it('module returns a run function', () => {
         expect(typeof lib).to.equal('function')
     })
+    it('run function accepts three params', () => {
+        expect(lib.length).to.equal(3)
+    })
+    it('run function accepts commander, logger and whenDone params', () => {
+        const commander = {}
+        commander.arguments = desc => {
+            expect(desc).to.equal('<filename>.json')
+            return commander
+        }
+        commander.action = onAction => {
+            expect(typeof onAction).to.equal('function')
+            return commander
+        }
+        commander.parse = processArgv => {
+            expect(processArgv).to.not.equal(null)
+            return commander
+        }
+        const logger = val => {
+            expect(val.length).to.greaterThan(0)
+        }
+        const keys = [
+            'filename',
+            'parsed',
+            'errors',
+            'contains',
+            'get'
+        ]
+        const whenDone = val => {
+            expect(Object.keys(val)).to.deep.equal(keys)
+        }
+        lib(commander, logger, whenDone)
+    })
 })
